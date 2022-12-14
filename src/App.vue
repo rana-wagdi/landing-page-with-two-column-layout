7f33c985696a50ad51ca1ea5ecec8abc51e25d2b
Rana Wagdi <>
2022-12-14 13:22:27 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- <header-items></header-items> -->
+    <main-items></main-items>
+</template>
+
+<script>
+// import HeaderItems from "./components/HeaderItems.vue";
+import MainItems from './components/MainItems.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HeaderItems,
+    MainItems
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainItems.vue
@@ -0,0 +1,29 @@
+<template>
+  <header-items></header-items>
+
+  <section class="container main_content">
+    <div>
+      <h1>All your files in one secure location, accessible anywhere.</h1>
+      <p>
+        Fylo stores your most important files in one secure location. Access
+        them wherever you need, share and collaborate with friends, family, and
+        co-workers.
+      </p>
+      <form>
+        <input type="email" placeholder="Enter your email..."/>
+        <button>Get Started</button>
+        <p class="error_email"></p>
+      </form>
+    </div>
+    <div>
+      <img src="../assets/illustration-1.svg" width="700" height="500"/>
+    </div>
+  </section>
+</template>
+<script>
+import HeaderItems from "./HeaderItems.vue";
+
+export default {
+  components: { HeaderItems },
+};
+</script>
